acf0e1d6bd15bb6bea23b782a0eb6739e30ce8ef
liav656levi-pixel <liav656levi@gmail.com>
2026-02-23 19:03:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
